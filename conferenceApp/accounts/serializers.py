46f971d08eedea258f53198eb2e19ff7c142ac05
goodness5 <goodnesskolapo@gmail.com>
2023-12-23 10:38:43 +0100

    fixed signin

---
+++ conferenceApp/accounts/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class SignupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+
+class SigninSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class ResetPasswordSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+
+class CompletePasswordResetSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    token = serializers.CharField()
+    new_password = serializers.CharField(write_only=True)
+    
