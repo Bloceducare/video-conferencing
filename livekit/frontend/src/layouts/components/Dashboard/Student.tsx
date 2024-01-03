39320a73c456d28c28c943f360a15befc3c2691e
MarsIfeanyi <marschalice@gmail.com>
2024-01-03 22:57:07 +0100

    Create Dashboard components

---
+++ livekit/frontend/src/layouts/components/Dashboard/Student.tsx
@@ -0,0 +1,69 @@
+import MeetingCard from '@/layouts/components/MeetingCard';
+import PageHeader from '@/layouts/partials/PageHeader';
+import { MeetingCardType } from '@/types';
+
+const Student = () => {
+  const meetingCards: MeetingCardType[] = [
+    {
+      title: 'Web3 Stack',
+      cohort: 'Cohort X',
+      room: 'Room I',
+      description: 'Live Webinar',
+      image: '/images/webinar.png',
+      link: '#',
+    },
+
+    {
+      title: 'Web2 Stack',
+      cohort: 'Cohort X',
+      room: 'Room II',
+      description: 'Live Webinar',
+      image: '/images/webinar.png',
+      link: '#',
+    },
+
+    {
+      title: 'Masterclass',
+      cohort: 'Cartesi',
+      room: 'Room III',
+      description: 'Live Webinar',
+      image: '/images/webinar.png',
+      link: '#',
+    },
+
+    {
+      title: 'General',
+      cohort: 'Web3bridge',
+      room: 'Room IV',
+      description: 'Live Webinar',
+      image: '/images/webinar.png',
+      link: '#',
+    },
+
+    {
+      title: 'Staff',
+      cohort: 'Web3bridge',
+      room: 'Room V',
+      description: 'Live Webinar',
+      image: '/images/webinar.png',
+      link: '#',
+    },
+  ];
+  return (
+    <>
+      <section>
+        <h3 className="flex justify-center text-center mt-4 ">Meeting Rooms</h3>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-8 px-6 mx-4 mt-10 ">
+          {meetingCards.map((card: MeetingCardType, index: number) => (
+            <div key={index} className="p-6 border rounded-xl shadow-md ">
+              <MeetingCard data={card} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Student;
