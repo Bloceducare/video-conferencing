a2b13ddc845a37687b332a3945ceee799efc08d7
MarsIfeanyi <marschalice@gmail.com>
2024-01-03 22:58:11 +0100

    create Student dashboard

---
+++ livekit/frontend/src/app/dashboard/student.tsx
@@ -0,0 +1,14 @@
+import Student from '@/components/Dashboard/Student';
+import React from 'react';
+
+type Props = {};
+
+const student = (props: Props) => {
+  return (
+    <div>
+      <Student />
+    </div>
+  );
+};
+
+export default student;
