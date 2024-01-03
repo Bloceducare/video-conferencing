52ab85ced1713d87d43124b221a6504cf5460f71
James Osunkiyesi <jaydhales@gmail.com>
2024-01-03 06:26:44 +0100

    feat: built custom control bar

--- livekit/frontend/src/layouts/components/CustomLiveKitComponents/CustomControlBar/index.tsx
+++ livekit/frontend/src/layouts/components/CustomLiveKitComponents/CustomControlBar/index.tsx
@@ -1,18 +1,21 @@
-import * as React from "react";
-import {useMaybeLayoutContext} from "@livekit/components-react";
-import {useLocalParticipantPermissions, usePersistentUserChoices} from "@livekit/components-react";
-import {supportsScreenSharing} from "@livekit/components-core";
-import {TrackToggle} from "@livekit/components-react";
-import {Track} from "livekit-client";
-import {MediaDeviceMenu} from "@livekit/components-react";
-import {ChatToggle} from "@livekit/components-react";
-import {ChatIcon, LeaveIcon} from "@livekit/components-react";
-import {DisconnectButton} from "@livekit/components-react";
-import {ControlBarProps} from "@livekit/components-react";
-import {useMediaQuery} from "usehooks-ts";
-import {mergeProps} from "../others/mergeProps";
-import {StartMediaButton} from "./StartMediaButton";
-
+import * as React from 'react';
+import { useMaybeLayoutContext } from '@livekit/components-react';
+import {
+  useLocalParticipantPermissions,
+  usePersistentUserChoices,
+} from '@livekit/components-react';
+import { supportsScreenSharing } from '@livekit/components-core';
+import { TrackToggle } from '@livekit/components-react';
+import { Track } from 'livekit-client';
+import { MediaDeviceMenu } from '@livekit/components-react';
+import { ChatToggle } from '@livekit/components-react';
+import { ChatIcon, LeaveIcon } from '@livekit/components-react';
+import { DisconnectButton } from '@livekit/components-react';
+import { ControlBarProps } from '@livekit/components-react';
+import { useMediaQuery } from 'usehooks-ts';
+import { mergeProps } from '../others/mergeProps';
+import { StartMediaButton } from './StartMediaButton';
+import { theme } from '@/config';
 
 /**
  * The `ControlBar` prefab gives the user the basic user interface to control their
@@ -31,135 +34,141 @@ import {StartMediaButton} from "./StartMediaButton";
  * @public
  */
 export default function CustomControlBar({
-                               variation,
-                               controls,
-                               saveUserChoices = true,
-                               ...props
-                           }: ControlBarProps) {
-    const [isChatOpen, setIsChatOpen] = React.useState(false);
-    const layoutContext = useMaybeLayoutContext();
-    React.useEffect(() => {
-        if (layoutContext?.widget.state?.showChat !== undefined) {
-            setIsChatOpen(layoutContext?.widget.state?.showChat);
-        }
-    }, [layoutContext?.widget.state?.showChat]);
-    const isTooLittleSpace = useMediaQuery(`(max-width: ${isChatOpen ? 1000 : 760}px)`);
-
-    const defaultVariation = isTooLittleSpace ? 'minimal' : 'verbose';
-    variation ??= defaultVariation;
-
-    const visibleControls = { leave: true, ...controls };
-
-    const localPermissions = useLocalParticipantPermissions();
-
-    if (!localPermissions) {
-        visibleControls.camera = false;
-        visibleControls.chat = false;
-        visibleControls.microphone = false;
-        visibleControls.screenShare = false;
-    } else {
-        visibleControls.camera ??= localPermissions.canPublish;
-        visibleControls.microphone ??= localPermissions.canPublish;
-        visibleControls.screenShare ??= localPermissions.canPublish;
-        visibleControls.chat ??= localPermissions.canPublishData && controls?.chat;
+  variation,
+  controls,
+  saveUserChoices = true,
+  ...props
+}: ControlBarProps) {
+  const [isChatOpen, setIsChatOpen] = React.useState(false);
+  const layoutContext = useMaybeLayoutContext();
+  React.useEffect(() => {
+    if (layoutContext?.widget.state?.showChat !== undefined) {
+      setIsChatOpen(layoutContext?.widget.state?.showChat);
     }
+  }, [layoutContext?.widget.state?.showChat]);
+  const isTooLittleSpace = useMediaQuery(`(max-width: ${isChatOpen ? 1000 : 760}px)`);
 
-    const showIcon = React.useMemo(
-        () => variation === 'minimal' || variation === 'verbose',
-        [variation],
-    );
-    const showText = React.useMemo(
-        () => variation === 'textOnly' || variation === 'verbose',
-        [variation],
-    );
+  const defaultVariation = isTooLittleSpace ? 'minimal' : 'verbose';
+  variation ??= defaultVariation;
 
-    const browserSupportsScreenSharing = supportsScreenSharing();
+  const visibleControls = { leave: true, ...controls };
 
-    const [isScreenShareEnabled, setIsScreenShareEnabled] = React.useState(false);
+  const localPermissions = useLocalParticipantPermissions();
 
-    const onScreenShareChange = React.useCallback(
-        (enabled: boolean) => {
-            setIsScreenShareEnabled(enabled);
-        },
-        [setIsScreenShareEnabled],
-    );
+  if (!localPermissions) {
+    visibleControls.camera = false;
+    visibleControls.chat = false;
+    visibleControls.microphone = false;
+    visibleControls.screenShare = false;
+  } else {
+    visibleControls.camera ??= localPermissions.canPublish;
+    visibleControls.microphone ??= localPermissions.canPublish;
+    visibleControls.screenShare ??= localPermissions.canPublish;
+    visibleControls.chat ??= localPermissions.canPublishData && controls?.chat;
+  }
 
-    const htmlProps = mergeProps({ className: 'lk-control-bar' }, props);
+  const showIcon = React.useMemo(
+    () => variation === 'minimal' || variation === 'verbose',
+    [variation]
+  );
+  const showText = React.useMemo(
+    () => variation === 'textOnly' || variation === 'verbose',
+    [variation]
+  );
 
-    const {
-        saveAudioInputEnabled,
-        saveVideoInputEnabled,
-        saveAudioInputDeviceId,
-        saveVideoInputDeviceId,
-    } = usePersistentUserChoices({ preventSave: !saveUserChoices });
+  const browserSupportsScreenSharing = supportsScreenSharing();
 
-    const microphoneOnChange = React.useCallback(
-        (enabled: boolean, isUserInitiated: boolean) =>
-            isUserInitiated ? saveAudioInputEnabled(enabled) : null,
-        [saveAudioInputEnabled],
-    );
+  const [isScreenShareEnabled, setIsScreenShareEnabled] = React.useState(false);
 
-    const cameraOnChange = React.useCallback(
-        (enabled: boolean, isUserInitiated: boolean) =>
-            isUserInitiated ? saveVideoInputEnabled(enabled) : null,
-        [saveVideoInputEnabled],
-    );
+  const onScreenShareChange = React.useCallback(
+    (enabled: boolean) => {
+      setIsScreenShareEnabled(enabled);
+    },
+    [setIsScreenShareEnabled]
+  );
 
-    return (
-        <div {...htmlProps}>
-            {visibleControls.microphone && (
-                <div className="lk-button-group">
-                    <TrackToggle
-                        source={Track.Source.Microphone}
-                        showIcon={showIcon}
-                        onChange={microphoneOnChange}
-                    >
-                        {showText && 'Microphone'}
-                    </TrackToggle>
-                    <div className="lk-button-group-menu">
-                        <MediaDeviceMenu
-                            kind="audioinput"
-                            onActiveDeviceChange={(_kind, deviceId) => saveAudioInputDeviceId(deviceId ?? '')}
-                        />
-                    </div>
-                </div>
-            )}
-            {visibleControls.camera && (
-                <div className="lk-button-group">
-                    <TrackToggle source={Track.Source.Camera} showIcon={showIcon} onChange={cameraOnChange}>
-                        {showText && 'Camera'}
-                    </TrackToggle>
-                    <div className="lk-button-group-menu">
-                        <MediaDeviceMenu
-                            kind="videoinput"
-                            onActiveDeviceChange={(_kind, deviceId) => saveVideoInputDeviceId(deviceId ?? '')}
-                        />
-                    </div>
-                </div>
-            )}
-            {visibleControls.screenShare && browserSupportsScreenSharing && (
-                <TrackToggle
-                    source={Track.Source.ScreenShare}
-                    captureOptions={{ audio: true, selfBrowserSurface: 'include' }}
-                    showIcon={showIcon}
-                    onChange={onScreenShareChange}
-                >
-                    {showText && (isScreenShareEnabled ? 'Stop screen share' : 'Share screen')}
-                </TrackToggle>
-            )}
-            {visibleControls.chat && (
-                <ChatToggle>
-                    {showIcon && <ChatIcon />}
-                    {showText && 'Chat'}
-                </ChatToggle>
-            )}
-            {visibleControls.leave && (
-                <DisconnectButton>
-                    {showIcon && <LeaveIcon />}
-                    {showText && 'Leave'}
-                </DisconnectButton>
-            )}
-            <StartMediaButton />
+  const htmlProps = mergeProps(
+    {
+      className:
+        'border-2 border-border dark:border-darkmode-border rounded-xl p-1 py-2 sm:p-4 flex items-center sm:gap-10 justify-between sm:justify-center mt-2',
+    },
+    props
+  );
+
+  const {
+    saveAudioInputEnabled,
+    saveVideoInputEnabled,
+    saveAudioInputDeviceId,
+    saveVideoInputDeviceId,
+  } = usePersistentUserChoices({ preventSave: !saveUserChoices });
+
+  const microphoneOnChange = React.useCallback(
+    (enabled: boolean, isUserInitiated: boolean) =>
+      isUserInitiated ? saveAudioInputEnabled(enabled) : null,
+    [saveAudioInputEnabled]
+  );
+
+  const cameraOnChange = React.useCallback(
+    (enabled: boolean, isUserInitiated: boolean) =>
+      isUserInitiated ? saveVideoInputEnabled(enabled) : null,
+    [saveVideoInputEnabled]
+  );
+
+  return (
+    <div {...htmlProps}>
+      {visibleControls.microphone && (
+        <div className="lk-button-group">
+          <TrackToggle
+            source={Track.Source.Microphone}
+            showIcon={showIcon}
+            onChange={microphoneOnChange}
+          >
+            {showText && 'Microphone'}
+          </TrackToggle>
+          <div className="lk-button-group-menu">
+            <MediaDeviceMenu
+              kind="audioinput"
+              onActiveDeviceChange={(_kind, deviceId) => saveAudioInputDeviceId(deviceId ?? '')}
+            />
+          </div>
         </div>
-    );
+      )}
+      {visibleControls.camera && (
+        <div className="lk-button-group">
+          <TrackToggle source={Track.Source.Camera} showIcon={showIcon} onChange={cameraOnChange}>
+            {showText && 'Camera'}
+          </TrackToggle>
+          <div className="lk-button-group-menu">
+            <MediaDeviceMenu
+              kind="videoinput"
+              onActiveDeviceChange={(_kind, deviceId) => saveVideoInputDeviceId(deviceId ?? '')}
+            />
+          </div>
+        </div>
+      )}
+      {visibleControls.screenShare && browserSupportsScreenSharing && (
+        <TrackToggle
+          source={Track.Source.ScreenShare}
+          captureOptions={{ audio: true, selfBrowserSurface: 'include' }}
+          showIcon={showIcon}
+          onChange={onScreenShareChange}
+        >
+          {showText && (isScreenShareEnabled ? 'Stop screen share' : 'Share screen')}
+        </TrackToggle>
+      )}
+      {visibleControls.chat && (
+        <ChatToggle>
+          {showIcon && <ChatIcon />}
+          {showText && 'Chat'}
+        </ChatToggle>
+      )}
+      {visibleControls.leave && (
+        <DisconnectButton>
+          {showIcon && <LeaveIcon />}
+          {showText && 'Leave'}
+        </DisconnectButton>
+      )}
+      <StartMediaButton />
+    </div>
+  );
 }
