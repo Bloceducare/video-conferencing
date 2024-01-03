51c247268a2851f189271abef076b89351e621e5
MarsIfeanyi <marschalice@gmail.com>
2024-01-03 23:05:46 +0100

    Render Student Dashboard

--- livekit/frontend/src/app/dashboard/page.tsx
+++ livekit/frontend/src/app/dashboard/page.tsx
@@ -1,31 +1,9 @@
-import MeetingCard from "@/layouts/components/MeetingCard";
-import PageHeader from "@/layouts/partials/PageHeader";
-import { MeetingCardType } from "@/types";
+import Students from './student';
 
 const MeetingCards = () => {
-  const meetingCards: MeetingCardType[] = [
-    {
-      title: "Web3Track",
-      description:
-        "Live Webinar",
-      image: "/images/webinar.png",
-      link: "#"
-    }
-  ];
   return (
     <>
-      <PageHeader />
-      <section className="section-sm pb-0">
-        <div className="container">
-          <div className="row justify-center">
-            {meetingCards.map((card: MeetingCardType, index: number) => (
-              <div className="mb-14 md:col-6 lg:col-4" key={index}>
-                <MeetingCard data={card} />
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
+      <Students />
     </>
   );
 };
