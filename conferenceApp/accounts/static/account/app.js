1258f639b1c0b1c2f543f6a0a7095d911a1a18b4
Ernesto <chidiebereernest8@gmail.com>
2024-01-06 07:51:43 +0100

    resolve conflict

--- conferenceApp/accounts/static/account/app.js
+++ conferenceApp/accounts/static/account/app.js
@@ -1,51 +1,51 @@
 function toggleMenu() {
-    let menuButtons = document.querySelector('.menu-buttons');
-    let toggleIcon = document.getElementById('toggleIcon');
-    let cancelIcon = document.getElementById('cancelIcon');
+  let menuButtons = document.querySelector('.menu-buttons');
+  let toggleIcon = document.getElementById('toggleIcon');
+  let cancelIcon = document.getElementById('cancelIcon');
 
-    // menuButtons.classList.toggle('active');
-    // toggleIcon.style.display = menuButtons.classList.contains('active') ? 'none' : 'block';
-    // cancelIcon.style.display = menuButtons.classList.contains('active') ? 'block' : 'none';
+  // menuButtons.classList.toggle('active');
+  // toggleIcon.style.display = menuButtons.classList.contains('active') ? 'none' : 'block';
+  // cancelIcon.style.display = menuButtons.classList.contains('active') ? 'block' : 'none';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    const signinForm = document.getElementById('signin-form');
+  const signinForm = document.getElementById('signin-form');
 
-    signinForm.addEventListener('submit', function (event) {
-        event.preventDefault();
+  signinForm.addEventListener('submit', function (event) {
+    event.preventDefault();
 
-        const username = document.getElementById('Username').value;
-        const password = document.getElementById('password').value;
-        // const rememberMe = document.querySelector('[name="remember-me"]').checked;
+    const username = document.getElementById('Username').value;
+    const password = document.getElementById('password').value;
+    // const rememberMe = document.querySelector('[name="remember-me"]').checked;
 
-        const formData = {
-            username: username,
-            password: password,
-            // remember_me: rememberMe,
-        };
+    const formData = {
+      username: username,
+      password: password,
+      // remember_me: rememberMe,
+    };
 
-        fetch('/accounts/signin/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Handle successful response
-            alert('login success')
-            window.location.href = ''
-            console.log(data);
-        })
-        .catch(error => {
-            // Handle errors
-            console.error('There was a problem with the fetch operation:', error);
-        });
-    });
+    fetch('/accounts/signin/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Handle successful response
+        alert('login success');
+        window.location.href = '';
+        console.log(data);
+      })
+      .catch((error) => {
+        // Handle errors
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  });
 });
