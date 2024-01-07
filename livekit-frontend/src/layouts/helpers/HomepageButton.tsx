73fc42cd8ec224b14b7450983692e71ba86df405
Samson Ajulor <samsonajulor@gmail.com>
2024-01-07 08:36:06 +0100

    added reg and login redirects

--- livekit-frontend/src/layouts/helpers/HomepageButton.tsx
+++ livekit-frontend/src/layouts/helpers/HomepageButton.tsx
@@ -1,15 +1,22 @@
 'use client'
+
+import Link from "next/link";
+
 // import { Button } from '@/types';
 
 const HomepageButton = () => {
   return (
     <div className="flex justify-center gap-1">
-      <button className="btn text-white hover:text-black bg-black  hover:bg-white border-solid border-white ">
-        Register
-      </button>
-      <button className="btn text-black hover:text-white bg-white  hover:bg-black border-solid border-black hover:border-white ">
-        Login
-      </button>
+      <Link href="https://web3bridgeauth.onrender.com/accounts/signup/">
+        <button className="btn text-white hover:text-black bg-black  hover:bg-white border-solid border-white ">
+          Register
+        </button>
+      </Link>
+      <Link href="https://web3bridgeauth.onrender.com/accounts/login/">
+        <button className="btn text-black hover:text-white bg-white  hover:bg-black border-solid border-black hover:border-white ">
+          Login
+        </button>
+      </Link>
     </div>
   );
 };
