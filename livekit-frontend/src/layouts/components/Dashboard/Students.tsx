c4b2cf6612c4ec8ff688de4ca7e10bcac516d38a
Samson Ajulor <samsonajulor@gmail.com>
2024-01-07 07:02:11 +0100

    remove unwanted text

--- livekit-frontend/src/layouts/components/Dashboard/Students.tsx
+++ livekit-frontend/src/layouts/components/Dashboard/Students.tsx
@@ -13,18 +13,18 @@ const Students = () => {
       <section>
         <h3 className="flex justify-center text-center mt-4 ">Meeting Rooms</h3>
         <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-16 px-14 mx-4 mt-10 ">
-          {meetingCardData.length &&
+          {!!meetingCardData.length &&
             meetingCardData?.map((card: MeetingCardType, index: number) => (
               <div key={index}>
                 <MeetingCard data={card} />
               </div>
             ))}
-          {!meetingCardData.length && (
-            <div key={1} className="text-center px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-              <p>No Meeting Yet</p>
-            </div>
-          )}
         </div>
+        {!meetingCardData.length && (
+          <div className="text-center px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+            <p>No Meeting Yet</p>
+          </div>
+        )}
       </section>
     </>
   );
